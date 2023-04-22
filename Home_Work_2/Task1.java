5bdbfb09c306df3a664d31061f2c7bb0d79ec050
JUSTero <skorneta@mail.ru>
2023-04-22 11:08:52 +0300

    Task 1 complete

--- Home_Work_2/Task1.java
+++ Home_Work_2/Task1.java
@@ -1,5 +1,8 @@
 package Home_Work_2;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Task1
  * Дана строка sql-запроса "select * from students where ". Сформируйте часть
@@ -9,13 +12,36 @@ package Home_Work_2;
  * 
  * Параметры для фильтрации: {"name":"Ivanov", "country":"Russia",
  * "city":"Moscow", "age":"null"}
- * 
- * Реализуйте алгоритм сортировки пузырьком числового массива, результат после
- * каждой итерации запишите в лог-файл.
  */
 
 public class Task1 {
     public static void main(String[] args) {
-        
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Ukraine");
+        map.put("city", "Kiev");
+        map.put("age", null);
+        map.put("name1", "Ivanov");
+        map.put("name2", "Ivanov");
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        if (params == null || params.isEmpty())
+            return result.toString();
+
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getKey() == null || pair.getValue() == null)
+                continue;
+
+            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+
+        if (result.length() > 5)
+            result.delete(result.length() - 5, result.length());
+
+        return result.toString();
     }
 }
