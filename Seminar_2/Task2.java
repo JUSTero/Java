4b44acf23b22f6f7829fcda851ded61a97d0ab52
JUSTero <skorneta@mail.ru>
2023-04-22 09:52:03 +0300

    Task 2 complete

--- Seminar_2/Task2.java
+++ Seminar_2/Task2.java
@@ -2,12 +2,38 @@ package Seminar_2;
 
 /**
  * Task2
- * 
+ * Напишите метод, который сжимает строку.
+ * Пример: вход aaaabbbcdd.
  */
 
 public class Task2 {
 
-    public static void main(String[] args) {
+    private static String squizeString(String str) {
+        StringBuilder build = new StringBuilder();
+        int count = 1;
 
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                if (count == 1) {
+                    build.append(str.charAt(i));
+                } else {
+                    build.append(str.charAt(i) + Integer.toString(count));
+                    count = 1;
+                }
+            }
+        }
+        if (count == 1) {
+            build.append(str.substring(str.length() - 1));
+        } else {
+            build.append(str.substring(str.length() - 1) + Integer.toString(count));
+        }
+        return build.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaaabbbcdd";
+        System.out.println(squizeString(str));
     }
 }
